56d80892e1199c6aa28824ce21839719a335301d
Setiady <setiady.wiguna@gmail.com>
2016-05-20 14:35:34 +0700

    add podfile

---
+++ Jiggie/Jiggie/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Jiggie
+//
+//  Created by Setiady Wiguna on 5/20/16.
+//  Copyright © 2016 Jiggie. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
